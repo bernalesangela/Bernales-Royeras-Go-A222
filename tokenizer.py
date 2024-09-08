f411548b318b10cf4997503fb60fdec5619968e6
bernalesangela <bernalesangela15@gmail.com>
2024-09-09 00:46:08 +0800

    Nawala na ang Delimiter token

--- tokenizer.py
+++ tokenizer.py
@@ -9,7 +9,6 @@ class TokenType:
     Whitespace = "Whitespace"
     Mixed = "Mixed"  
 
-
 class Token:
     def __init__(self, type_, value):
         self.type = type_
@@ -17,7 +16,6 @@ class Token:
 
     def __repr__(self):
         return f"Token: '{self.value}' \t\t Type: '{self.type}'"
-
 
 def categorize(token):
     has_alpha = any(char.isalpha() for char in token)
@@ -58,8 +56,6 @@ def tokenize(inputString):
                 token_type = categorize(tokenVal)
                 tokens.append(Token(token_type, tokenVal))
                 newArr.clear()
-
-            tokens.append(Token(TokenType.Delimiter, ch))
             i += 1
 
         elif ch == '\n':
@@ -83,7 +79,6 @@ def tokenize(inputString):
 
     return tokens
 
-
 def granularBreakdown(tokens):
     print("\n===============================================")
     print("\nGranular Breakdown:")
@@ -96,13 +91,12 @@ def granularBreakdown(tokens):
         
         print("No tokens generated.")
 
-
 def main():
     sampleText1 = "123angela:bernales789"
     sampleText2 = "Hello, world! Version 2.0 is here on 2024-08-27."
-    sampleText3 = "delimiter:_:test\n:New line here:"
-    sampleText4 = "123456:numbers:and:some:words:?"
-    sampleText5 = ";"
+    sampleText3 = "-2:"
+    sampleText4 = ""
+    sampleText5 = "Hello world!: blah:2:15@:\n:\nhello:\n:"
 
     sampleTexts = [sampleText1, sampleText2, sampleText3, sampleText4, sampleText5]
 
@@ -126,4 +120,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
